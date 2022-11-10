50c96da6c5b758d62e776d466536b1e3ec6fa127
Caleb Miller <caleb@mathmammoth.com>
2022-11-10 12:26:16 -0600

    render

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const child_process = require("child_process");
 const fs = require("fs");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 10000;
 
 app.use(express.json());
 
